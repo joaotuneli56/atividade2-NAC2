b35fc0cc50f1a1bae86a7fee52e6ddc3c2c42ddd
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-14 20:33:26 -0300

    Atividade 3 - teste

---
+++ Test/ProdutoControllerTest.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Newtonsoft.Json;
+using web_app_domain;
+using web_app_performance.Controllers;
+using web_app_repository;
+using Xunit;
+
+namespace Test
+{
+    public class ProdutoControllerTest
+    {
+        private readonly Mock<IProdutoRepository> _produtoRepositoryMock;
+        private readonly ProdutoController _controller;
+
+        public ProdutoControllerTest()
+        {
+            _produtoRepositoryMock = new Mock<IProdutoRepository>();
+            _controller = new ProdutoController(_produtoRepositoryMock.Object);
+        }
+
+        [Fact]
+        public async Task Get_ListarProdutosOk()
+        {
+            // Arrange 
+            var produtos = new List<Produto>() {
+                new Produto()
+                {
+                    Id = 1,
+                    Nome = "Produto A",
+                    Preco = 10.0m
+                }
+            };
+            _produtoRepositoryMock.Setup(r => r.ListarProdutos()).ReturnsAsync(produtos);
+
+            // Act
+            var result = await _controller.GetProduto();
+
+            // Assert
+            Assert.IsType<OkObjectResult>(result);
+            var okResult = result as OkObjectResult;
+            Assert.Equal(JsonConvert.SerializeObject(produtos), JsonConvert.SerializeObject(okResult.Value));
+        }
+
+        [Fact]
+        public async Task Get_ListarRetornaNotFound()
+        {
+            _produtoRepositoryMock.Setup(p => p.ListarProdutos()).ReturnsAsync((IEnumerable<Produto>)null);
+
+            var result = await _controller.GetProduto();
+
+            Assert.IsType<NotFoundResult>(result);
+        }
+
+        [Fact]
+        public async Task Post_SalvarProduto()
+        {
+            // Arrange
+            var produto = new Produto()
+            {
+                Id = 1,
+                Nome = "Produto B",
+                Preco = 20.0m
+            };
+            _produtoRepositoryMock.Setup(p => p.SalvarProduto(It.IsAny<Produto>()))
+                .Returns(Task.CompletedTask);
+
+            // Act
+            var result = await _controller.Post(produto);
+
+            // Assert
+            _produtoRepositoryMock
+                .Verify(p => p.SalvarProduto(It.IsAny<Produto>()), Times.Once);
+            Assert.IsType<OkObjectResult>(result);
+        }
+    }
+}
---
+++ Test/ProdutoRepositoryTest.cs
@@ -0,0 +1,64 @@
+﻿using Moq;
+using web_app_domain;
+using web_app_performance.Controllers;
+using web_app_repository;
+using Xunit;
+
+namespace Test
+{
+    public class ProdutoRepositoryTest
+    {
+        [Fact]
+        public async Task ListarProdutos()
+        {
+            // Arrange
+            var produtos = new List<Produto>() {
+                new Produto()
+                {
+                    Id = 1,
+                    Nome = "Produto A",
+                    Preco = 10.0m
+                },
+                new Produto()
+                {
+                    Id = 2,
+                    Nome = "Produto B",
+                    Preco = 20.0m
+                }
+            };
+            var produtoRepositoryMock = new Mock<IProdutoRepository>();
+            produtoRepositoryMock.Setup(p => p.ListarProdutos()).ReturnsAsync(produtos);
+            var produtoRepository = produtoRepositoryMock.Object;
+
+            // Act 
+            var result = await produtoRepository.ListarProdutos();
+
+            // Assert
+            Assert.Equal(produtos, result);
+        }
+
+        [Fact]
+        public async Task SalvarProduto()
+        {
+            // Arrange
+            var produto = new Produto()
+            {
+                Id = 1,
+                Nome = "Produto C",
+                Preco = 30.0m
+            };
+            var produtoRepositoryMock = new Mock<IProdutoRepository>();
+            produtoRepositoryMock
+                .Setup(p => p.SalvarProduto(It.IsAny<Produto>()))
+                .Returns(Task.CompletedTask);
+            var produtoRepository = produtoRepositoryMock.Object;
+
+            // Act
+            await produtoRepository.SalvarProduto(produto);
+
+            // Assert
+            produtoRepositoryMock
+                .Verify(p => p.SalvarProduto(It.IsAny<Produto>()), Times.Once);
+        }
+    }
+}
